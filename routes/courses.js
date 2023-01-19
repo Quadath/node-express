dce94f25a7d51fd0a32f1150e622e5c575c8078c
Quadatz <quadath@gmail.com>
2023-01-20 00:32:20 +0300

    course page

--- routes/courses.js
+++ routes/courses.js
@@ -3,13 +3,21 @@ const Course = require('../models/course')
 const router = Router()
 
 router.get('/', async (req, res) => {
-    const courses = await Course.getAll()
-    res.render('courses', {
-        title: 'Courses',
-        isCourses: true,
-        courses
-    })
- })
+  const courses = await Course.getAll()
+  res.render('courses', {
+    title: 'Курсы',
+    isCourses: true,
+    courses
+  })
+})
+
+router.get('/:id', async (req, res) => {
+  const course = await Course.getById(req.params.id)
+  res.render('course', {
+    layout: 'empty',
+    title: `Курс ${course.title}`,
+    course
+  })
+})
 
 module.exports = router
- 
