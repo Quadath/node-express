15def5a5c2d6a6feeb917340ec63f98957e67154
Quadatz <quadath@gmail.com>
2023-01-19 10:23:30 +0300

    separate routes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
-const { extname } = require('path')
 
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
 
 const app = express()
 
@@ -16,28 +18,15 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static('public'))
+app.use('/', homeRoutes)
+app.use('/add',addRoutes)
+app.use('/courses', coursesRoutes)
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Home page',
-        isHome: true
-    })
-})
 
-app.get('/add', (req, res) => {
-   res.render('add', {
-    title: 'Add course',
-    isAdd: true
-   })
-})
 
-app.get('/courses', (req, res) => {
-    res.render('courses', {
-        title: 'Courses',
-        isCourses: true
-    })
- })
- 
+
+
+
 
 const PORT = process.env.PORT || 3000;
 
