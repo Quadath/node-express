94d43f1fc2d4aaa7d2d212ef81f46b507c1723e1
Quadatz <quadath@gmail.com>
2023-01-26 15:07:32 +0300

    user model

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,7 @@ const {Router} = require('express')
 const router = Router()
 
 router.get('/', (req, res) => {
+  console.log(req)
   res.render('index', {
     title: 'Главная страница',
     isHome: true
