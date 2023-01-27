697e94b62cab4686992a9e70b66fbc9476b28db5
Quadatz <quadath@gmail.com>
2023-01-27 15:20:53 +0300

    cart displaying

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,6 @@ const {Router} = require('express')
 const router = Router()
 
 router.get('/', (req, res) => {
-  console.log(req)
   res.render('index', {
     title: 'Главная страница',
     isHome: true
