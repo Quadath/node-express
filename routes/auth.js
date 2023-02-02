e60ea3512a91f66d4742445d37c3161076d359b8
Quadatz <quadath@gmail.com>
2023-02-02 14:43:38 +0300

    registering and authorization

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 const router = Router()
 
@@ -10,15 +11,51 @@ router.get('/login', async(req, res) => {
 })
 
 router.post('/login', async(req, res) => {
-    const user = await User.findById('63da812b560fec748cf1072f')
-    req.session.user = user
-    req.session.isAuthenticated = true
-    req.session.save(err => {
-        if(err) {
-            throw err
+    try{
+        const {email, password} = req.body
+
+        const candidate = await User.findOne({email}) 
+        if(candidate) {
+            const areSame = await bcrypt.compare(password, candidate.password)
+            if(areSame) {
+                req.session.user = candidate
+                req.session.isAuthenticated = true
+                req.session.save(err => {
+                    if(err) {
+                        throw err
+                    }
+                    res.redirect('/')
+                })
+            } else {
+                res.redirect('/auth/login#login')
+            }
+        } else {
+            res.redirect('/auth/login#login')
         }
-        res.redirect('/')
-    })
+    } catch(err) {
+        console.log(e)
+    }
+  
+})
+
+router.post('/register', async (req, res) => {
+    try {
+        const {email, password, repeat, name} = req.body
+        const candidate = await User.findOne({ email })
+
+        if(candidate) {
+            res.redirect('/auth/login#login')
+        } else {
+            const hashPassword = await bcrypt.hash(password, 12)
+            const user = new User({
+                email, password: hashPassword, name, cart: {items: []}
+            })
+            await user.save()
+            res.redirect('/auth/login#login')
+        }
+    } catch (e) {
+        console.log(e)
+    }
 })
 
 router.get('/logout', async(req, res) => {
